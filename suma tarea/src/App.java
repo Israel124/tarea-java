531ce5602115f3a37d89c80578c834a6a023a7c6
israel <israeldejesusespinozafranco@gmail.com>
2024-04-27 13:09:32 -0500

    cuarto cambio, tarea completada

--- suma tarea/src/App.java
+++ suma tarea/src/App.java
@@ -15,14 +15,19 @@ public class App {
         do {
 
             try {
+
                 System.out.println("Ingrese tres numeros enteros diferentes");
                 num1 = leer.nextInt();
                 num2 = leer.nextInt();
                 num3 = leer.nextInt();
+
             } catch (Exception e) {
+
                 System.out.println("Ingrese valores correctos");
                 leer.nextLine();
                 continue;
+
+             //a esta parte dentro del catch le agregue la condicion de que si los numero esran iguales diera un mensaje indicando que ellos tenian que ser diferentes
 
             }   if (num1 == num2 || num1 == num3 || num2 == num3) {
                 System.out.println("Los numeros deben de ser difetentes");
@@ -31,9 +36,11 @@ public class App {
               }
 
             break;
+
         } while (true);
 
-
+     //sumar los números con menor valor, para esto difiní una variable llamada suma como entero
+     //y con un ifelse se se realizó la suma
 
         int suma;
 
@@ -50,9 +57,28 @@ public class App {
             suma = num3 + (num1 <= num2? num1 : num2);
         }
 
-        System.out.println("La suma de los números de menor valor es: " + suma);
+        System.out.println("La suma de los números con menor valor es: " + suma);
     
-        
+       //diferencia o resta de los numeros con mayor valor, para eso se definieron 3 variables como entero
+       //la primera variable llamada"diferencia", la segunda "maximo1" y la tercera "maximo 2"
+       //para realizar esta poeracion hice uso de la clase Math.max esta me ayudo a localizar cuales de los valores ingresados era mayor
+       //luego serealizo la operación donde lo que se queria obtener era la diferencia por lo cual, diferencia= maximo1 - maximo2
 
+        int diferencia;
+
+        int maximo1 = Math.max(num1, Math.max(num2, num3));
+        int maximo2 = Math.min(Math.max(num1, num2), Math.max(num2, num3));
+
+        diferencia = maximo1 - maximo2;
+
+        System.out.println("La direfencia o resta de los números con mayor valor es: " + diferencia);
+
+      //número con mayor valor, para esto declramos una variable como entero llamada "maximo" 
+      // con la clase Math.max encontramos cual de los 3 numeros ingresados es el mayor
+
+       int maximo = Math.max(Math.max(num1, num2), num3);
+
+        System.out.println("El número con mayor valor es: " + maximo);
+          
     }
 }
