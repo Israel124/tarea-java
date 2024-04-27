b9a7d0cdb85fd3a414d73ff373dc3ad8d40f78ef
israel <israeldejesusespinozafranco@gmail.com>
2024-04-27 09:32:41 -0500

    tercer cambio

--- suma tarea/src/App.java
+++ suma tarea/src/App.java
@@ -8,9 +8,8 @@ public class App {
     public static int num1;
     public static int num2;
     public static int num3;
-
     public static void main(String[] args) throws Exception {
-        int suma;
+        
         Scanner leer = new Scanner(System.in);
 
         do {
@@ -21,30 +20,39 @@ public class App {
                 num2 = leer.nextInt();
                 num3 = leer.nextInt();
             } catch (Exception e) {
-                System.out.println(e);
                 System.out.println("Ingrese valores correctos");
                 leer.nextLine();
-            }
+                continue;
 
-        } while (num1 == num2 || num1 == num3 || num2 == num3);
+            }   if (num1 == num2 || num1 == num3 || num2 == num3) {
+                System.out.println("Los numeros deben de ser difetentes");
+                continue;
+  
+              }
 
-        if (num2 >= num1 && num2 >= num3 && num3 >= num1) {
+            break;
+        } while (true);
 
-            suma = num1+num2;
 
-        } else if (num1 == num2 || num1 == num3 || num2 == num3) {
-           
-            suma = num2+num3;
-            
-        }else if (num1 == num2 || num1 == num3 || num2 == num3) {
-           
-            suma = num1+num3;
-            
-        } else{
-            System.out.println("los valores no se pueden sumar porque son iguales ó no son enteros");
+
+        int suma;
+
+        if (num1 <= num2 && num1 <= num3) {
+
+            suma = num1 + (num2 <= num3? num2 : num3);
+
+        } else if (num2 <= num1 && num2 <= num3) {
+
+            suma = num2 + (num1 <= num3? num1 : num3);
+
+        } else {
+
+            suma = num3 + (num1 <= num2? num1 : num2);
         }
 
-        System.out.println("la suma de los numeros de menor valor es: " + suma);
+        System.out.println("La suma de los números de menor valor es: " + suma);
+    
+        
 
     }
 }
